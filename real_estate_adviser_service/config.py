1d990d7b274ca6c47ffd1abe833667e573066b77
roman-bilets <roman.bilets@teck.com>
2024-04-22 21:44:16 -0700

    Updated config

--- real_estate_adviser_service/config.py
+++ real_estate_adviser_service/config.py
@@ -12,8 +12,12 @@ class Config:
         self.hist_start_year = int(os.getenv("HISTORICAL_START_YEAR"))
         self.hist_batch_incr_days = int(os.getenv("HISTORICAL_BATCH_INCREMENT_DAYS"))
         self.active_listing_days = int(os.getenv("LOAD_ACTIVE_LISTINGS_DAYS"))
-        
 
+        self.db_host = os.getenv("DB_HOST")
+        self.db_name = os.getenv("DB_NAME")
+        self.db_port = os.getenv("DB_PORT")
+        self.db_username = os.getenv("DB_USERNAME")
+        self.db_password = os.getenv("DB_PASSWORD")
 
 
 config = Config()
